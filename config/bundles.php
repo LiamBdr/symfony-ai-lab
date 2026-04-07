d765f9f684167f7642f35d931cd06492cca9a778
Liam <84729003+LiamBdr@users.noreply.github.com>
2026-04-07 23:40:59 +0200

    feat: add Symfony AI bundle with Ollama platform integration

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
 ];
